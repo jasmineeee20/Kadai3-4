cf60e4674a42e9cd9c2a1c221056d24f1b70bd23
g15905fm <g15905fm@s30536.tsuda.ac.jp>
2017-05-11 18:25:56 +0900

    テストケースを作り、プログラムを変更しました

--- snake-camel/test/snakecamel/SnakeCamelUtilTest.java
+++ snake-camel/test/snakecamel/SnakeCamelUtilTest.java
@@ -1,5 +1,6 @@
 package snakecamel;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,9 +8,23 @@ import org.junit.Test;
 public class SnakeCamelUtilTest {
 
 	@Test
-	public void test() {
+	public void スネークをキャメルに変換されてるか() {
+		SnakeCamelUtil snake = new SnakeCamelUtil();
+		//String expected = "AbcDefGhi";
+		String actual = snake.snakeToCamelcase("snake_case");
 		
-		System.out.println("Not yet implemented");
+		//assertThat(,is(expected));
+		System.out.println(actual);
+	}
+	@Test
+	public void キャメルをスネークに変換されてるか() {
+		SnakeCamelUtil camel = new SnakeCamelUtil();
+		String actual2= camel.camelToSnakecase("CamelCase");
+		System.out.println(actual2);
+	}
+	@Test
+	public void test() {
+		System.out.println("not yet implemented");
 	}
 
 }
